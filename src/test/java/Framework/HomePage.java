6167f21d8eb2cb39b6aba9791056b2d9165684aa
nuruliislam <mdnrl59@gmail.com>
2020-10-15 04:15:46 +0100

    parameterizing the tests with multiple data

--- src/test/java/Framework/HomePage.java
+++ src/test/java/Framework/HomePage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -15,8 +16,8 @@ public class HomePage extends base
 	public WebDriver driver;
 	
 	
-	@Test
-	public void initialize() throws IOException
+	@Test(dataProvider="getUsers")
+	public void initialize(String email,String pass) throws IOException
 	{
 	
 		 driver = initializeDriver();
@@ -26,11 +27,27 @@ public class HomePage extends base
 		 lp.getLoginPage().click();
 		 
 		 LoginPage lg=new LoginPage(driver);
-		 lg.getEmail().sendKeys("your email");
-		 lg.getPassword().sendKeys("your password");
+		 lg.getEmail().sendKeys(email);
+		 lg.getPassword().sendKeys(pass);
 		 
 		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		 lg.getLogin().click();
-
+	}
+	
+	//provide multiple data
+	@DataProvider
+	public Object[][] getUsers()
+	{
+		
+		Object[][] data=new Object[2][2];
+		//0th row
+		data[0][0]="1st user email";
+		data[0][1]="1st user password";
+		
+		//1st row
+		data[1][0]="2nd user email";
+		data[1][1]="2nd user password";
+		
+		return data;
 	}
 }
