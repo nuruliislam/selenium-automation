7347656bd39dd406386294f68bbd655e18988e5c
nuruliislam <mdnrl59@gmail.com>
2020-10-16 09:44:34 +0100

    added BeforeTest and AfterTest annotation

--- src/test/java/Framework/validateTitle.java
+++ src/test/java/Framework/validateTitle.java
@@ -3,16 +3,24 @@ package Framework;
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pageObjects.LandingPage;
 import resources.base;
 
-public class validateTitle extends base {
-public WebDriver driver;
+public class validateTitle extends base 
+{
+	public WebDriver driver;
 	
+	@BeforeTest
+	public void initialize() throws IOException
+	{
+		driver =initializeDriver();
+	}
 	
 	@Test
-	public void initialize() throws IOException
+	public void basePageNavigation() throws IOException
 	{
 	
 		 driver = initializeDriver();
@@ -21,5 +29,11 @@ public WebDriver driver;
 		 LandingPage lp=new LandingPage(driver);
 		 Assert.assertEquals(lp.getTitle().getText(),"Welcome to Bikroy.com - the largest marketplace in Bangladesh!");
 
+	}
+	
+	@AfterTest
+	public void teardown()
+	{
+		driver.close();
 	}
 }
