0d53b269393808e242cc3604f04423071daa0eb4
nuruliislam <mdnrl59@gmail.com>
2020-10-16 11:43:00 +0100

    implement log4j in Listeners class

--- src/test/java/Framework/Listeners.java
+++ src/test/java/Framework/Listeners.java
@@ -1,29 +1,33 @@
 package Framework;
 
-import java.io.IOException;
-import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import resources.base;
 
-public class Listeners extends base implements ITestListener {
+public class Listeners extends base implements ITestListener 
+{
+	public static Logger log =LogManager.getLogger(base.class.getName());
 
 	@Override
 	public void onTestStart(ITestResult result) {
-		// TODO Auto-generated method stub
+		log.info("Test Started");
 		
 	}
 
 	@Override
-	public void onTestSuccess(ITestResult result) {
-		// TODO Auto-generated method stub
+	public void onTestSuccess(ITestResult result) 
+	{
 		
+		log.info("Test Succeded");
 	}
 
 	@Override
-	public void onTestFailure(ITestResult result) {
-		// TODO Auto-generated method stub
+	public void onTestFailure(ITestResult result) 
+	{
+		log.info("Test Failed");
 		
 	}
 
