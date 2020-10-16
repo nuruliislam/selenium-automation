0e409569c095894b2d0282779d6a251ed2e928b1
nuruliislam <mdnrl59@gmail.com>
2020-10-16 10:59:09 +0100

    implement log4j

--- src/test/java/Framework/validateTitle.java
+++ src/test/java/Framework/validateTitle.java
@@ -1,6 +1,9 @@
 package Framework;
 
 import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -12,22 +15,26 @@ import resources.base;
 public class validateTitle extends base 
 {
 	public WebDriver driver;
+	public static Logger log =LogManager.getLogger(base.class.getName());
 	
 	@BeforeTest
 	public void initialize() throws IOException
 	{
 		driver =initializeDriver();
+		log.info("Driver is initialized");
 	}
 	
 	@Test
 	public void basePageNavigation() throws IOException
 	{
 	
-		 driver = initializeDriver();
+		 
 		 driver.get(prop.getProperty("url"));
+		 log.info("navigate to homepage");
 		 
 		 LandingPage lp=new LandingPage(driver);
 		 Assert.assertEquals(lp.getTitle().getText(),"Welcome to Bikroy.com - the largest marketplace in Bangladesh!");
+		 log.info("Successfully validated Title ");
 	}
 	
 	@AfterTest
